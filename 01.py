6f16192124f41aeb5e8d227c0a1cfa0435e810c4
matix0508 <51704650+matix0508@users.noreply.github.com>
2020-02-24 01:45:50 +0100

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,17 @@
+LIMIT = 1000
+
+def multiple(num):
+    if num % 5 == 0 or num % 3 == 0:
+        return True
+    else:
+        return False
+
+def main():
+    output = 0
+    for i in range(LIMIT):
+        if multiple(i):
+            output += i
+    print(output)
+
+if __name__ == "__main__":
+    main()
