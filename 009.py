7d70620fe1f55d65af072334d5bc25ee856c0d8f
matix0508 <matix0508@gmail.com>
2020-03-07 12:21:49 +0100

    task 9 finished

---
+++ 009.py
@@ -0,0 +1,11 @@
+def main():
+    for a in range(1, 1000):
+        for b in range(1, 1000):
+            c = 1000 - a - b
+            if a ** 2 + b ** 2 == c **2:
+                print(a * b * c)
+                return 0
+
+
+if __name__ == "__main__":
+    main()
