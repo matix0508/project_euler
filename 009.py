e5117603e99a0f5065048b9d432522d3dc10ab92
wleklinski <matix0508@gmail.com>
2020-03-09 12:21:24 +0100

    prints a, b, c

--- 009.py
+++ 009.py
@@ -3,6 +3,7 @@ def main():
         for b in range(1, 1000):
             c = 1000 - a - b
             if a ** 2 + b ** 2 == c **2:
+                print((a, b, c))
                 print(a * b * c)
                 return 0
 
